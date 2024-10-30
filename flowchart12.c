285d23641cced4b86a453a6798a38d95c7234229
nilosarma <nilosarma8@gmail.com>
2024-10-30 12:49:22 +0530

    Update

---
+++ flowchart12.c
@@ -0,0 +1,12 @@
+#include<stdio h>
+int main()
+{
+int A,B;
+printf("Enter two numbers");
+scanf("%d%d",&A,&B);
+if(A<B)
+  printf("%d is smaller",A);
+else
+  printf("%d is smaller",B);
+return 0;
+}
